481e36db12fffab1f0f21b5e322235e61ff68b30
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-16 23:50:22 +0100

    updating

--- authFlow/src/types/AllInterfaces.ts
+++ authFlow/src/types/AllInterfaces.ts
@@ -21,6 +21,24 @@ export interface ICard {
     route1 : string;
     text1 : string
     path1 : string
+
+
+    tex?: boolean;
+    reg?: boolean;
+    textTitle?: string;
+    message?: string;
+    remake?: string;
+
+    email?: string;
+    password?: string;
+    userName?: string;
+
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+
+
+  setEmail?: React.Dispatch<React.SetStateAction<string>>;
+  setPassword?: React.Dispatch<React.SetStateAction<string>>;
+  setUserName?: React.Dispatch<React.SetStateAction<string>>;
 } 
 export interface Iinput{
     title1 : string;
@@ -28,4 +46,30 @@ export interface Iinput{
     title3? : string;
     sign : boolean;
     reset : boolean;
+
+    email?: string;
+    password?: string;
+    userName?: string;
+  
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
+
+    setEmail?: React.Dispatch<React.SetStateAction<string>>;
+    setPassword?: React.Dispatch<React.SetStateAction<string>>;
+    setUserName?: React.Dispatch<React.SetStateAction<string>>;
 } 
+
+export interface iGlobal {
+    email?: string;
+    password?: string;
+    userName?: string;
+    setUserName?: React.Dispatch<React.SetStateAction<string>>;
+    setPassword?: React.Dispatch<React.SetStateAction<string>>;
+    setEmail?: React.Dispatch<React.SetStateAction<string>>;
+  }
+
+  export interface iText {
+    textTitle?: string;
+    message?: string;
+    remake?: string;
+  }
+  
